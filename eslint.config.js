7dc668dfc2890bb59d9273ad048ce0c86386d6a6
xsh <xushihai@talkweb.com.cn>
2024-08-28 18:04:13 +0800

    配置eslint

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { ignores: ["node_modules", "dist", "public"] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      indent: ["error", 2], // 缩进使用 2 个空格
+      "linebreak-style": ["error", "unix"], // 使用 Unix 风格的换行符
+      quotes: ["error", "single"], // 使用单引号
+      semi: ["error", "never"], // 语句末尾不加分号
+      "no-console": process.env.NODE_ENV === "production" ? "warn" : "off", // 生产环境中警告 console 使用，开发环境中关闭规则
+      "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off", // 生产环境中警告 debugger 使用，开发环境中关闭规则
+    },
+  },
+];
