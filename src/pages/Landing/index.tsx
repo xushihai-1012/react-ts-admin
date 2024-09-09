f412c082a33e2ecae5568d978c53bbcb60dc2f51
xsh <xushihai@talkweb.com.cn>
2024-09-09 17:55:30 +0800

    通过landing页面测试全局主题修改功能

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,9 +1,26 @@
 import useSettingStore from '@/stores/settings'
-import { Typography } from 'antd'
+import { Typography, Button, ColorPicker, DatePicker, Flex } from 'antd'
+
+const { Title } = Typography
+
 const LandingPage: React.FC = () => {
   const { colorPrimary, setColorPrimary } = useSettingStore()
 
-  return <div></div>
+  return (
+    <div>
+      <Title level={3}>landing page</Title>
+      <Flex gap={16}>
+        <Button type="primary">primary</Button>
+        <Button>default</Button>
+        <DatePicker.RangePicker></DatePicker.RangePicker>
+        <ColorPicker
+          showText
+          value={colorPrimary}
+          onChange={(color) => setColorPrimary(color.toHex())}
+        ></ColorPicker>
+      </Flex>
+    </div>
+  )
 }
 
 export default LandingPage
